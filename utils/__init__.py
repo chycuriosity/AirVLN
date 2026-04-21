20d3f06443d0318fb2e6623a70d66e7f808c67ad
chycuriosity <3349026630@qq.com>
2026-04-21 16:32:03 +0800

    Add local utils package marker to avoid import conflicts

---
+++ utils/__init__.py
@@ -0,0 +1,7 @@
+"""Project-local utils package.
+
+This file ensures imports like ``from utils.logger import logger`` resolve to
+the repository code instead of third-party ``utils`` packages that may be
+installed in the Python environment.
+"""
+
